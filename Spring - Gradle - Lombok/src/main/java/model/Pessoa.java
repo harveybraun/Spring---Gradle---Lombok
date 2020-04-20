14a47bbee1899970e64c5315f8e5f9201cbade57
Harvey Braun <63766520+harveybraun@users.noreply.github.com>
2020-04-19 21:03:45 -0300

    Add files via upload

--- Spring - Gradle - Lombok/src/main/java/model/Pessoa.java
+++ Spring - Gradle - Lombok/src/main/java/model/Pessoa.java
@@ -1,5 +1,12 @@
 package model;
 
+/**
+ * Descreve a Classe Pessoa
+ *
+ * @author Harvey Braun
+ * @version 1.0
+ * @since 18/04/2020 - 14:00
+ */
 public class Pessoa {
 
     private String nome;
